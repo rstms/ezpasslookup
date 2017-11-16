00fd5f091da98ea636ab90991807aac918ccec29
Matt Krueger <mkrueger@rstms.net>
2017-11-16 11:40:21 -0600

    rewrite using selenium and headless-chrome

--- Makefile
+++ Makefile
@@ -11,14 +11,19 @@ $(PROJECT).zip:	$(PROJECT).py
 clean:
 	rm *.zip
 
-test1:
-	python3 ezpaylookup.py T021775658460 FEB6625
+test:	test0 test1 test2 test3
 
-test9:
-	python3 ezpaylookup.py T021784575788 T720852C
+test0:
+	python3 ezpaylookup.py bad_violation bad_plate | jq .
+
+test1:
+	python3 ezpaylookup.py T021775658460 FEB6625 | jq .
 
 test2:
-	python3 ezpaylookup.py T021783673301 T737609C
+	python3 ezpaylookup.py T021783673301 T737609C | jq .
+
+test3:
+	python3 ezpaylookup.py T021784575788 T720852C | jq .
 
 install:
 	aws lambda delete-function --region=$(AWS_REGION) --function-name=$(PROJECT)
--- ezpaylookup.py
+++ ezpaylookup.py
@@ -1,41 +1,24 @@
 #!/usr/bin/env python3
 
-import requests
-import time
-from bs4 import BeautifulSoup
-import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-from incapsula import IncapSession
+import json
+
+import time
+import sys
 
 URL = 'https://www.e-zpassny.com'
 
-ERROR_UNKNOWN = 'ERROR-Unknown'
-ERROR_NODATA = 'ERROR-No Violations Match'
+ERROR_UNKNOWN = '"ERROR-Unknown"'
+ERROR_NODATA = '"ERROR-No Violations Match"'
 
-TIMEOUT = 60
-DELAY_INTERVAL = .1
-STANDOFF_MULTIPLIER = 1.01
-RETRY_COUNT = 64 
+TOTAL_KEY = 'Total No of Violations:'
+INFO_KEY = 'Violation Information'
+ERROR_KEY = 'No violations match the information you entered.'
 
-FIELDS = [
- 'org.apache.struts.taglib.html.TOKEN',
- 'formid',
- 'transponderNumber'
-]
-
-
-def get_page(s, url):
-    print('GET %s' % url)
-    r = s.get(url)
-    print('status=%d' % r.status_code)
-    if r.status_code == 200:
-        return r
-    return None
-
-def remove_parms(url):
-    if '?' in url:
-            url = url.split('?')[0]
-    return url
+KEYS = ['Violation No.', 'License Plate', 'Date & Time', 'Facility', 'Status', 'Toll', 'Fee', 'Amt Due']
 
 def lambda_handler(event, context):
 
@@ -43,134 +26,94 @@ def lambda_handler(event, context):
     violation_number = event['violation_number']
     license_plate = event['license_plate']
 
-    r = None
-    tries = 0
-    start_time = time.time()
-    standoff = DELAY_INTERVAL
-    while (tries < RETRY_COUNT) and (time.time() - start_time < TIMEOUT):
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
 
-        # wait longer before each retry
-        tries += 1
-        if tries > 1:
-            standoff *= STANDOFF_MULTIPLIER
-            print('Sleeping %.2f seconds...' % standoff)
-            time.sleep(standoff)
+    driver = webdriver.Chrome(
+        executable_path='/usr/lib/chromium-browser/chromedriver',
+        chrome_options = chrome_options
+    )
 
-        s = requests.Session() 
-        #s = IncapSession()
-        print('%d Session %s' % (tries, s))
+    vurl = 'vector/violations/violationInquiry.do'
+    driver.get('%s/%s' % (url, vurl))
 
-        # get home page first
-        r = get_page(s, url)
-        if not r:
-            continue
+    if not 'E-ZPass' in driver.title:
+        driver.close()
+        return ERROR_UNKNOWN
 
-        #soup = BeautifulSoup(r.text, 'lxml')
-        #print(soup.prettify())
+    e = driver.find_element_by_name('violationNumber')
+    e.clear()
+    e.send_keys(violation_number)
 
-        vurl = 'vector/violations/violationInquiry.do'
-        #vurl = 'vector/violations/violationInquiry.do?locale=en_US&from=Home'
-        #vurl = 'payviolation'
+    e = driver.find_element_by_name('licensePlate')
+    e.clear()
+    e.send_keys(license_plate)
 
-        r = get_page(s, '%s/%s' % (url, vurl))
-        if not r:
-            continue
+    driver.find_element_by_name('btnSearch').click()
 
-        soup = BeautifulSoup(r.text, 'lxml')
+    if ERROR_KEY in driver.page_source:
+        driver.close()
+        return ERROR_NODATA
 
-        m = soup.find_all('meta')
-        if False:
-            print('meta: %s' % m)
-            url2 = [c['content'].split(';')[1] for c in m if 'url=' in c['content']][0].split('=')[1]
-            r = get_page(s, remove_parms(url2))
-            if not r:
-                continue
-            soup = BeautifulSoup(r.text, 'lxml')
+    # find number of violations
+    items = []
+    while True:
 
-        f = soup.form
-        inputs = f.find_all('input')
-
-        form_data = {}
-    
-        for i in inputs:
-            print('input: %s' % i['name'])
-            if i['name'] in FIELDS:
-                form_data[i['name']] = i['value']
-
-        form_data['violationNumber'] = violation_number
-        form_data['licensePlate'] = license_plate
-        form_data['licenseState'] = 'NY'
-        form_data['licenseCountry'] = 'USA'
-        form_data['btnSearch.x'] = 49
-        form_data['btnSearch.y'] = 6
-
-        for line in [l for l in soup.prettify().split('\n') if 'hidElement.' in l]:
-            #print('element: %s' % line)
-            if '"value"' in line:
-                form_data['ctokenElem'] = line.split(',')[1].split("'")[1]
-
-        action = f['action']
-
-        #print('%s%s' % (url, action))
-        #for k,v in form_data.items():
-        #    print('%s: %s' % (k,v))
-        
-        post_url = '%s%s' % (url, remove_parms(action))
-
-        print('POST %s' % post_url)
-        #r = s.post(post_url, data=form_data)
-        r = s.post(post_url, data=form_data, proxies={'https': 'localhost:8080'}, verify=False)
-        print('status=%s' % r.status_code)
-
-        if r.status_code == 200:
+        if not TOTAL_KEY in driver.page_source:
+            ret = ERROR_UNKNOWN
             break
+
+        trows = [e.text for e in driver.find_elements_by_class_name('fd')]
+
+        if not TOTAL_KEY in trows:
+            ret = ERROR_UNKNOWN
+            break
+
+        tindex = trows.index(TOTAL_KEY)
+        total = int(trows[tindex+1])
+
+        if not trows[tindex+2].startswith(INFO_KEY):
+            ret = ERROR_UNKNOWN
+            break
+        
+        rows = trows[tindex+2].split('\n')
+
+        #for i in range(len(rows)):
+        #    print('%d %s' % (i, repr(rows[i])))
+
+        cnums = rows[0].split()
+
+        if cnums[2] == '1' and cnums[3] == 'item':
+            snum = 1
+            enum = 1
+            if total != 1:
+                ret = ERROR_UNKNOWN
+                break
         else:
-            r = None
+            snum = int(cnums[2])
+            enum = int(cnums[4])
+            if total != int(cnums[6]):
+                ret = ERROR_UNKNOWN
+                break
 
-    if not r:
-        print('Error: retries exhausted')
-        return ERROR_UNKNOWN
+        if snum != len(items) + 1:
+            ret = ERROR_UNKNOWN
+            break
+    
+        count = (enum - snum) + 1
+        for i in range(0, count):
+            item = {}
+            for j in range(0, 8):
+                item[KEYS[j]] = rows[2 + i * 8 + j].strip()
+            items.append(item)
 
-    soup = BeautifulSoup(r.text, 'lxml')
+        if len(items) < total:
+            driver.find_element_by_xpath("//a[@title='Goto next page']").click()
+        else:    
+            ret = json.dumps(items, sort_keys=True)
+            break
 
-    # find key table defs; these aren't marked well, so drill into the dom to find them
-    try:
-        ktds = soup.find_all('tbody')[1].tr.td.table.tr.td.table.tr.find_all('td')
-    except IndexError:
-        print('ERROR: tbody parse error: %s' % soup.prettify())
-        return ERROR_UNKNOWN
-
-    keys = []
-    for td in ktds[1:]:
-        #print('%s' % td.text.strip())
-        keys.append(td.text.strip())
-
-    #print('keys: %s' % repr(keys))
-    expected_keys = ['Violation No.', 'License Plate', 'Date & Time', 'Facility', 'Status', 'Toll', 'Fee', 'Amt Due']
-    if keys != expected_keys:
-        print('Error: keys mismatch: keys=%s, expected=%s' % (keys, expected_keys))
-        return ERROR_UNKNOWN
-
-    # the value table defs are all marked with a unique class, so find them that way
-    vtds = soup.form.find_all('td', attrs={'class': 'cl'})
-
-    #print('ktds %d %s' % (len(ktds), ktds))
-    #print('vtds %d %s' % (len(vtds), vtds))
-
-    # multiple rows of vtds may exist.  we assume len(vtds) % len(ktds) == 0
-    if len(vtds) % len(ktds):
-        print('Error: parsing vtds=%s' % repr(vtds))
-        return ERROR_UNKNOWN
-
-    ret=[]
-    for i in range(0, len(vtds), len(ktds)):
-        row = {}
-        j = i
-        for key in keys:
-            j+=1
-            row[key] = vtds[j].text.strip()
-        ret.append(row)
-            
+    driver.close()
     return ret
 
 if __name__ == '__main__':
@@ -180,4 +123,3 @@ if __name__ == '__main__':
     event['url'] = URL
     ret = lambda_handler(event, None)
     print('%s' % ret)
-    #import pdb; pdb.set_trace()
